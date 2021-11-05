4b1c279925fb7e3a879757462688993944bc69d0
ID56 <mashrurmahmud@iut-dhaka.edu>
2021-11-05 12:39:03 +0600

    Modified gVar

--- utils/augment.py
+++ utils/augment.py
@@ -1,10 +1,8 @@
 import albumentations as A
 import numpy as np
-from PIL import Image
 import cv2
-from typing import Tuple
 
-def grayscale_variation(image: np.ndarray, eta: int = 10, g_min: int = 155, g_max: int = 255, depth_range: Tuple[float, float] = (200, 1500)) -> np.ndarray:
+def grayscale_variation(image: np.ndarray, eta: int = 10, g_min: int = 155, g_max: int = 255, near_depth_thresh: int = 200) -> np.ndarray:
     """Quantizes depth levels into discrete grayscale image levels.
 
     Args:
@@ -12,24 +10,20 @@ def grayscale_variation(image: np.ndarray, eta: int = 10, g_min: int = 155, g_ma
         eta (int, optional): Number of gray levels. Defaults to 10.
         g_min (int, optional): Lowest gray level. Defaults to 155.
         g_max (int, optional): Highest gray level. Defaults to 255.
+        near_depth_thresh (int, optional): Minimum considered depth. Defaults to 200.
 
     Returns:
         np.ndarray: Depth quantized image.
     """
     
-    if depth_range != None:
-        near, far = depth_range
-        mask = np.logical_and(image > near, image < far)
-    else:
-        mask = image > 0
-
-    d_th = image.max()
+    d_th = max(1, image.max())
+    mask = image > near_depth_thresh
     
     if np.any(mask):
         d_min = image[mask].min()
     else:
-        d_min, d_th = 0, max(1, d_th)
-     
+        d_min = 0
+    
     g_stride = int((g_max - g_min) / eta)
     return np.where(mask, g_min + np.round(eta * (image - d_min) / (d_th - d_min)) * g_stride, 0).astype(np.uint8)
 
