03bdd6ac47edfad264f52fcd9fd52ce94fe2012b
ID56 <mashrurmahmud@iut-dhaka.edu>
2021-11-07 02:06:31 +0600

    Changed subnetwork order

--- models/fusion_network.py
+++ models/fusion_network.py
@@ -24,18 +24,17 @@ class FeatureFusionNet(nn.Module):
 
     def forward(self, x):
         x_jnt, x_dpt = x
-        t = x_dpt.shape[1]
-
-        # depth images
-        x_dpt = rearrange(x_dpt, "b t c h w -> (b t) c h w")
-        x_dpt = self.depth_cnn(x_dpt)
-        x_dpt = rearrange(x_dpt, "(b t) c h w -> b t (c h w)", t=t)
-        x_dpt = self.depth_lstm(x_dpt)[0]
 
         # 2d joints
         x_jnt = self.joint_lstm(x_jnt)[0]
 
-        # multimodal fusion
+        # depth images
+        x_dpt = rearrange(x_dpt, "b t c h w -> (b t) c h w")
+        x_dpt = self.depth_cnn(x_dpt)
+        x_dpt = rearrange(x_dpt, "(b t) c h w -> b t (c h w)", t=x_jnt.shape[1])
+        x_dpt = self.depth_lstm(x_dpt)[0]
+
+        # feature fusion
         x_fused = torch.cat((x_dpt, x_jnt), dim=1)
         x_fused = rearrange(x_fused, "b t f -> b (t f)")
         x_fused = self.mlp(x_fused)
@@ -62,19 +61,19 @@ class ScoreFusionNet(nn.Module):
 
     def forward(self, x):
         x_jnt, x_dpt = x
-        t = x_dpt.shape[1]
+        
+        # 2d joints
+        x_jnt = self.joint_lstm(x_jnt)[0]
 
         # depth images
         x_dpt = rearrange(x_dpt, "b t c h w -> (b t) c h w")
         x_dpt = self.depth_cnn(x_dpt)
-        x_dpt = rearrange(x_dpt, "(b t) c h w -> b t (c h w)", t=t)
+        x_dpt = rearrange(x_dpt, "(b t) c h w -> b t (c h w)", t=x_jnt.shape[1])
         x_dpt = self.depth_lstm(x_dpt)[0]
-        x_dpt = rearrange(x_dpt, "b t f -> b (t f)")
-        x_dpt = self.depth_mlp(x_dpt)
 
-        # 2d joints
-        x_jnt = self.joint_lstm(x_jnt)[0]
-        x_jnt = rearrange(x_jnt, "b t f -> b (t f)")
+        # score fusion
+        x_dpt, x_jnt = rearrange([x_dpt, x_jnt], "i b t f -> i b (t f)")
+        x_dpt = self.depth_mlp(x_dpt)
         x_jnt = self.joint_mlp(x_jnt)
         
         return 0.5 * (x_dpt + x_jnt)
@@ -85,13 +84,16 @@ def model_from_name(name, num_classes):
 
     if name == "gvar_feature_fusion":
         model = FeatureFusionNet(num_classes=num_classes)
+
     elif name == "gvar_score_fusion":
         model = ScoreFusionNet(num_classes=num_classes)
+
     elif name == "vanilla_feature_fusion":
         model = FeatureFusionNet(res_in=(227,227), num_classes=num_classes, drop_prb=0.0, mlp_layers=[256,512,256], lstm_units=256,
-                    use_bilstm=False, use_bn=False)
+                    use_bilstm=False, actn_type="relu", use_bn=False)
+                    
     elif name == "vanilla_score_fusion":
         model = ScoreFusionNet(res_in=(227,227), num_classes=num_classes, drop_prb=0.0, mlp_layers=[256,512,256], lstm_units=256,
-                    use_bilstm=False, use_bn=False)
+                    use_bilstm=False, actn_type="relu", use_bn=False)
 
     return model
