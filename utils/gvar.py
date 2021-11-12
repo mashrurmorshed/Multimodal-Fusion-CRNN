bbd28dff853fa38d130eadffa539e475ad97e960
ID56 <mashrurmahmud@iut-dhaka.edu>
2021-11-12 07:39:54 +0600

    Added far depth thresh

--- utils/gvar.py
+++ utils/gvar.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def grayscale_variation(image: np.ndarray, eta: int = 10, g_min: int = 155, g_max: int = 255, near_depth_thresh: int = 200) -> np.ndarray:
+def grayscale_variation(image: np.ndarray, eta: int = 10, g_min: int = 155, g_max: int = 255, near_depth_thresh: int = 0, far_depth_thresh: int = None) -> np.ndarray:
     """Quantizes depth levels into discrete grayscale image levels.
 
     Args:
@@ -8,19 +8,21 @@ def grayscale_variation(image: np.ndarray, eta: int = 10, g_min: int = 155, g_ma
         eta (int, optional): Number of gray levels. Defaults to 10.
         g_min (int, optional): Lowest gray level. Defaults to 155.
         g_max (int, optional): Highest gray level. Defaults to 255.
-        near_depth_thresh (int, optional): Minimum considered depth. Defaults to 200.
-
+        near_depth_thresh (int, optional): Minimum considered depth. Defaults to 0.
+        far_depth_thresh (int, optional): Maximum considered depth. Defaults to None.
     Returns:
         np.ndarray: Depth quantized image.
     """
     
-    d_th = max(1, image.max())
     mask = image > near_depth_thresh
+    if far_depth_thresh != None:
+        mask = np.logical_and(mask, image < far_depth_thresh)
     
+    d_max = max(1, image.max())
     if np.any(mask):
         d_min = image[mask].min()
     else:
         d_min = 0
     
     g_stride = int((g_max - g_min) / eta)
-    return np.where(mask, g_min + np.round(eta * (image - d_min) / (d_th - d_min)) * g_stride, 0).astype(np.uint8)
+    return np.where(mask, g_min + np.round(eta * (image - d_min) / (d_max - d_min)) * g_stride, 0).astype(np.uint8)
